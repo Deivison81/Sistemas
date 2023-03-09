8170707d8b1d960f3119ae4078a780dd622a3e2f
Deivison Jimenez <deivi.jimenez@gmail.com>
2023-03-09 00:18:55 -0400

    Capa negocio y Datos

--- Sistemas.Datos/DCategoria.cs
+++ Sistemas.Datos/DCategoria.cs
@@ -40,28 +40,164 @@ namespace Sistemas.Datos
         }
 
         public DataTable Buscar(string valor) 
-        { 
-        
+        {
+            SqlDataReader Resultado;
+            DataTable tabla = new DataTable();
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                sqlcon = Conexion.getInstancia().CrearConexion();
+
+                SqlCommand Comando = new SqlCommand("categoria_buscar", sqlcon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@valor", SqlDbType.VarChar).Value = valor;
+                sqlcon.Open();
+                Resultado = Comando.ExecuteReader();
+                tabla.Load(Resultado);
+                return tabla;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            }
+
+
         }
-        public string insertar(Categoria Obj) 
-        { 
-            
+        public string Insertar(Categoria Obj) 
+        {
+            string Rpta = "";
+            SqlConnection sqlcon = new SqlConnection();
+            try 
+            {
+                sqlcon = Conexion.getInstancia().CrearConexion();
+
+                SqlCommand Comando = new SqlCommand("categoria_insertar", sqlcon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@nombre", SqlDbType.VarChar).Value = Obj.Nombre;
+                Comando.Parameters.Add("@descripcion", SqlDbType.VarChar).Value = Obj.Descripcion;
+                sqlcon.Open();
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "No se puede ingresar el registro";
+            }
+            catch (Exception ex) 
+            {
+                throw ex;
+            } finally 
+            {
+
+                if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            }
+            return Rpta;
+
         }
         public string Actualizar(Categoria Obj) 
-        { 
-        
+        {
+            string Rpta = "";
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                sqlcon = Conexion.getInstancia().CrearConexion();
+
+                SqlCommand Comando = new SqlCommand("categoria_actualizar", sqlcon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@idcategoria", SqlDbType.Int).Value = Obj.Idcategoria;
+                Comando.Parameters.Add("@nombre", SqlDbType.VarChar).Value = Obj.Nombre;
+                Comando.Parameters.Add("@descripcion", SqlDbType.VarChar).Value = Obj.Descripcion;
+                sqlcon.Open();
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "No se puede Actualizar el registro";
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+
+                if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            }
+            return Rpta;
         }
         public string Eliminar(int id) 
-        { 
-        
+        {
+            string Rpta = "";
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                sqlcon = Conexion.getInstancia().CrearConexion();
+
+                SqlCommand Comando = new SqlCommand("categoria_eliminar", sqlcon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@idcategoria", SqlDbType.Int).Value = id;
+                
+                sqlcon.Open();
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "No se puede Eliminar el registro";
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+
+                if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            }
+            return Rpta;
         }
         public string Activar(int id) 
         {
-            
+            string Rpta = "";
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                sqlcon = Conexion.getInstancia().CrearConexion();
+
+                SqlCommand Comando = new SqlCommand("categoria_activar", sqlcon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@idcategoria", SqlDbType.Int).Value = id;
+
+                sqlcon.Open();
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "No se ha  podido Activar  el registro";
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+
+                if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            }
+            return Rpta;
         }
         public string Desativar(int id) 
-        { 
-        
+        {
+            string Rpta = "";
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                sqlcon = Conexion.getInstancia().CrearConexion();
+
+                SqlCommand Comando = new SqlCommand("categoria_desactivar", sqlcon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@idcategoria", SqlDbType.Int).Value = id;
+
+                sqlcon.Open();
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "No se ha  podido Desactivar  el registro";
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+
+                if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            }
+            return Rpta;
         }
 
     }
---
+++ Sistemas.Negocio/NCategoria.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Sistemas.Datos;
+using Sistemas.Entidades;
+
+namespace Sistemas.Negocio
+{
+    public class NCategoria
+    {
+        public static DataTable Listar() 
+        {
+            DCategoria Datos = new DCategoria();
+            return Datos.Listar();
+            
+        }
+
+        public static DataTable Buscar(string valor)
+        {
+            DCategoria Datos = new DCategoria();
+            return Datos.Buscar(valor);
+        }
+        public static string Insertar(string Nombre, string Descripcion) 
+        {
+            DCategoria Datos = new DCategoria();
+            Categoria Obj = new Categoria();
+            Obj.Nombre = Nombre;
+            Obj.Descripcion = Descripcion;
+
+            return Datos.Insertar(Obj);
+        }
+        public static string Actualizar(int id, string Nombre, string Descripcion)
+        {
+            DCategoria Datos = new DCategoria();
+            Categoria Obj = new Categoria();
+            Obj.Idcategoria = id;
+            Obj.Nombre = Nombre;
+            Obj.Descripcion = Descripcion;
+
+            return Datos.Actualizar(Obj);
+        }
+        public static string Eliminar(int id)
+        {
+            DCategoria Datos = new DCategoria();
+            
+            return Datos.Eliminar(id);
+
+        }
+        public static string Activar(int id)
+        {
+            DCategoria Datos = new DCategoria();
+
+            return Datos.Activar(id);
+        }
+        public static string Desactivar(int id)
+        {
+            DCategoria Datos = new DCategoria();
+            return Datos.Desativar(id);
+        }
+    }
+}
